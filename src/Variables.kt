83a1f7d704f8226c99a342d24f1fa75da8e53738
George Higbie <gehigbie@gmail.com>
2017-09-14 20:01:12 -0600

    Add more varaibles and string file

--- src/Variables.kt
+++ src/Variables.kt
@@ -1,3 +1,28 @@
 fun main(args: Array<String>) {
 
+    val name = "JonnyB"
+    var isAwesome = true
+
+    println("Is " + name + " awesome? The answer is: " + isAwesome)
+
+    var a = 3
+    var b = 6
+    println(a + b)
+
+    val doubleNum = 123.45  //64 bit number
+    val floatNum = 123.45f  //32 bit number //with floats 'f' must be added at the end
+
+    val longNum = 1233456779875L //64 bit number
+    val aDouble = a.toString();
+
+    //you can specifically name the variable type
+    val name1: String = "Pedro"
+    var x: Int = 4
+    var y: Int = 5
+    var doubleNumb2: Double = 1234.45
+
+    var hero: String
+    hero = "batman"
+    hero = "superman"
+    println(hero)
 }
