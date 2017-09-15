79dbcd3574950eb71ef8800a451783f481d9d278
George Higbie <gehigbie@gmail.com>
2017-09-14 20:42:11 -0600

    Add operators

---
+++ src/Numbers.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>){
+    val a = 12
+    val b = 25.3
+
+    println(b + a)
+    println(b - a)
+    println(b / a)
+    println(a * b)
+
+    val c = 25
+    println(c % a)
+}
